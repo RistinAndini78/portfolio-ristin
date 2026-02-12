36e26ef12ce84641bb7d5d48ed501b37e183984a
RistinAndini78 <ristinristin798@gmail.com>
2026-02-12 17:56:52 +0700

    Update

--- script.js
+++ script.js
@@ -96,10 +96,23 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         const target = document.querySelector(this.getAttribute("href"));
 
         if (target) {
-            window.scrollTo({
-                top: target.offsetTop - 120,
-                behavior: "smooth"
+            target.scrollIntoView({
+                behavior: "smooth",
+                block: "start"
             });
         }
     });
 });
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//     anchor.addEventListener("click", function (e) {
+//         e.preventDefault();
+//         const target = document.querySelector(this.getAttribute("href"));
+
+//         if (target) {
+//             window.scrollTo({
+//                 top: target.offsetTop - 120,
+//                 behavior: "smooth"
+//             });
+//         }
+//     });
+// });
